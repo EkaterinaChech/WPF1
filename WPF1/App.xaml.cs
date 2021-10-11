650998817bffc631813969bc50faa70381568aa9
Katya <katyachekhovskaya@gmail.com>
2021-10-12 01:02:54 +0300

    First commit - quiet much done actually

---
+++ WPF1/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System;
+using System.Windows.Threading;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public MainWindow window;
+        private void StartUp(object sender, StartupEventArgs e)
+        {
+            window = new MainWindow();
+            if (e.Args.Length == 1)
+            {
+                MessageBox.Show("Now opening file: \n\n" + e.Args[0]);
+            }
+
+            window.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Error: " + e.Exception.Message);
+            e.Handled = true;
+        }
+    }
+}
---
+++ WPF1/MinMaxClose.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF1
+{
+    /// <summary>
+    /// Interaction logic for MinMaxClose.xaml
+    /// </summary>
+    public partial class MinMaxClose : UserControl
+    {
+        public MinMaxClose()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+
+        private void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(App.Current.MainWindow);
+        }
+
+        private void MaximizeClick(object sender, RoutedEventArgs e)
+        {
+            if (App.Current.MainWindow.WindowState == WindowState.Maximized)
+                App.Current.MainWindow.WindowState = WindowState.Normal;
+            else
+                SystemCommands.MaximizeWindow(App.Current.MainWindow);
+        }
+    }
+}
