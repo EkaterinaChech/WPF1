9eced03c72fc53d6957b7368c5204ae6b360cfda
Katya <katyachekhovskaya@gmail.com>
2021-10-28 21:13:25 +0300

    Changed Theme launch

--- WPF1/App.xaml.cs
+++ WPF1/App.xaml.cs
@@ -16,7 +16,12 @@ namespace WPF1
     /// </summary>
     public partial class App : Application
     {
+<<<<<<< Updated upstream
         
+=======
+        private static string Theme = "Theme";
+
+>>>>>>> Stashed changes
         private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
         {
             MessageBox.Show("Error: " + e.Exception.Message);
@@ -25,6 +30,18 @@ namespace WPF1
 
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+<<<<<<< Updated upstream
+=======
+            StreamReader reader = File.OpenText(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent + "\\Properties\\launchSettings.json");
+            
+            JObject o = (JObject)JToken.ReadFrom(new JsonTextReader(reader));
+            reader.Close();
+            string theme = (string)o[Theme];
+            //Resources = new ResourceDictionary() { Source = new Uri($"\\Resources\\{theme}Theme.xaml", UriKind.Relative) };
+            Current.Resources.MergedDictionaries.Add(
+                LoadComponent(new Uri($"\\Resources\\{theme}Theme.xaml", UriKind.Relative)) as ResourceDictionary);
+
+>>>>>>> Stashed changes
             new MainWindow().Show();
 
         }
